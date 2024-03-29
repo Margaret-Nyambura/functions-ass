81701759781f8333e3847976e3553dce2b6b872c
Margaret-Nyambura <margaretmacharianyambura@gmail.com>
2024-03-29 12:48:01 +0300

    added files

---
+++ functions.js
@@ -0,0 +1,23 @@
+// Given an array of strings, use a function to reverse all the elements in the string in ascending order and the specific elements in descending order
+
+function sort(){
+    let names = ["Ann", "Mary", "Susan", "John"]
+    console.log(names.sort());
+    console.log(names.reverse());
+}
+
+// Using JS functions and an array of numbers, return positive if an element within the array is positive, negative if an element is negative, else zero
+
+function loopThrough(){
+    let array1 = array1.forEach(element => {
+        if (element>0){
+            console.log("positive");
+        }else if(element>0){
+            console.log("negative");
+        }else{
+            console.log("zero");
+        }
+    });
+
+
+}
